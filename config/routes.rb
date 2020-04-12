0222e44bf712161442cde5930fda5e10bae1d1ec
Haruka W <nekonoko1995@gmail.com>
2020-04-12 21:28:53 +0900

    [Add]invitesコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show, :edit, :update]
+  resources :invites
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/invites_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class InvitesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get invites_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get invites_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get invites_show_url
+    assert_response :success
+  end
+
+end

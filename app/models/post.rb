49baf0baee17a7f4ffc3a17cfbce96326903d348
Haruka W <nekonoko1995@gmail.com>
2020-04-13 17:52:51 +0900

    [Add]postsコントローラ作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,5 +12,10 @@ class Post < ApplicationRecord
 		"未選択": 0, "白": 1, "黒": 2, "グレー": 3, "三毛猫": 4, "茶トラ": 5, "キジトラ": 6, "サバトラ": 7, "サビ": 8, "白黒": 9,
 		"グレー白": 10, "茶白": 11, "キジ白": 12, "サバ白": 13, "その他": 14
 	}, _prefix: true
+	# [type]カラムでエラーが出るのを防ぐために記載
+	self.inheritance_column = :_type_disabled
+
+	belongs_to :user
+	attachment :image
 
 end

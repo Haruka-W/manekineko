a89828e898fc825bf1d2266e5112603a4578e349
Haruka W <nekonoko1995@gmail.com>
2020-04-14 21:53:03 +0900

    [Fix]usersテーブルprofileカラムをtext型からstring型へ変更

---
+++ db/migrate/20200414122611_change_profile_to_users.rb
@@ -0,0 +1,6 @@
+class ChangeProfileToUsers < ActiveRecord::Migration[5.2]
+  def change
+  	# カラム型をtextからstringに修正
+    change_column :users, :profile, :string, default: "こんにちは！", null: false
+  end
+end

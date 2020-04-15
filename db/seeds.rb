e4e56a961acf9be4279c29a15b7ef7059a8989d4
Haruka W <nekonoko1995@gmail.com>
2020-04-15 17:42:27 +0900

    [Add]シードデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# coding: utf-8
+
+# 会員のテストデータ
+User.create!(
+	email: "test@com",
+	name: "山田太郎",
+	handle_name: "テスト太郎",
+	profile: "テスト用会員です。",
+	prefecture: 13,
+	password: "123456"
+)
+
+# 募集のテストデータ10件
+10.times do |n|
+	Invite.create!(
+		user_id: 1,
+		title: "募集テスト タイトル#{n + 1}",
+		content: "募集内容テスト",
+		sex: 1,
+		age: 2,
+		type: 5,
+	)
+end
+
+# 投稿のテストデータ10件
+10.times do |n|
+	Post.create!(
+		user_id: 1,
+		title: "投稿テスト タイトル#{n + 1}",
+		content: "投稿内容テスト",
+		sex: 2,
+		age: 1,
+		type: 4,
+	)
+end

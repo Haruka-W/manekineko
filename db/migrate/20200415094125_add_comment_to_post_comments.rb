b3161f306d00cf3880114920353ab5b0357bb6db
Haruka W <nekonoko1995@gmail.com>
2020-04-15 18:53:05 +0900

    [Add]コメントのカラムを追加

---
+++ db/migrate/20200415094125_add_comment_to_post_comments.rb
@@ -0,0 +1,6 @@
+class AddCommentToPostComments < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :post_comments, :comment, :text
+  	add_column :invite_comments, :comment, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_122611) do
+ActiveRecord::Schema.define(version: 2020_04_15_094125) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_122611) do
     t.integer "invite_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "comment"
   end
 
   create_table "invites", force: :cascade do |t|
@@ -50,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_122611) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "comment"
   end
 
   create_table "posts", force: :cascade do |t|

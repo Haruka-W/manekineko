59f70fcb64311f84d3c5dd4f7f88f75293491913
Haruka W <nekonoko1995@gmail.com>
2020-04-18 20:02:41 +0900

    [Add]フラッシュメッセージを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
-    redirect_to user_path(current_user.id)
+    redirect_to user_path(current_user.id), flash: {success: "投稿が完了しました。"}
   end
 
   def edit
@@ -28,13 +28,13 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
     post.update(post_params)
-    redirect_to post_path(post)
+    redirect_to post_path(post), flash: {success: "変更が完了しました。"}
   end
 
   def destroy
     post = Post.find(params[:id])
     post.destroy
-    redirect_to user_path(current_user.id)
+    redirect_to user_path(current_user.id), flash: {success: "投稿を削除しました。"}
   end
 
   private

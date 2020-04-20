cf0afa063866c99b9a348ca9f4926019023645cf
Haruka W <nekonoko1995@gmail.com>
2020-04-20 15:43:01 +0900

    [Fix]非同期通信時フラッシュメッセージが遷移先に残る不具合を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	add_flash_types :success, :info, :warning, :danger
+	after_action :flash_message
 
 	protected
+
   	# 会員新規登録の際にデータ保存する為に以下のカラムを記載
   	def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :handle_name, :prefecture])
   	end
+
+  	# 非同期通信時のフラッシュメッセージが遷移先に残るのを防ぐため
+  	def flash_message
+  	  flash.discard
+  	end
+
 end

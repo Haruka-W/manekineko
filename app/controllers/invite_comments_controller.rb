e3d9e0d6e47c2489cc574070f6cde2f76f4d553d
Haruka W <nekonoko1995@gmail.com>
2020-04-20 14:39:51 +0900

    [Add]inviteの気になる・コメント機能を非同期通信に変更（未完成）

--- app/controllers/invite_comments_controller.rb
+++ app/controllers/invite_comments_controller.rb
@@ -1,12 +1,26 @@
 class InviteCommentsController < ApplicationController
 
 	def create
-		invite = Invite.find(params[:invite_id])
-    	comment = current_user.invite_comments.new(invite_comment_params)
-    	comment.invite_id = invite.id
-    	comment.save
-    	redirect_to invite_path(invite)
+		@invite = Invite.find(params[:invite_id])
+    	@invite_comment = @invite.invite_comments.new(invite_comment_params)
+    	@invite_comment.user_id = current_user.id
+    	if @invite_comment.save
+            flash[:success] = "コメントしました。"
+    		# redirect_to invite_path(@invite)
+    	else
+            flash[:danger] = "コメントを入力してください。"
+            @invite_comments = InviteComment.where(id: @invite)
+    		# redirect_to invite_path(@invite)
+    	end
 	end
+
+	def destroy
+        @invite_comment = InviteComment.find(params[:invite_id])
+        @invite = @invite_comment.invite
+        @invite_comment.destroy
+        flash[:success] = "コメントを削除しました。"
+        # redirect_to invite_path(@invite_comment.invite.id)
+    end
 
 	private
 
--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -1,17 +1,17 @@
 class MarksController < ApplicationController
 
 	def create
-		invite = Invite.find(params[:invite_id])
-        mark = current_user.marks.new(invite_id: invite.id)
+		@invite = Invite.find(params[:invite_id])
+        mark = current_user.marks.new(invite_id: @invite.id)
         mark.save
-        redirect_to invite_path(invite)
+        # redirect_to invite_path(invite)
 	end
 
 	def destroy
-		invite = Invite.find(params[:invite_id])
-        mark = current_user.marks.find_by(invite_id: invite.id)
+		@invite = Invite.find(params[:invite_id])
+        mark = current_user.marks.find_by(invite_id: @dinvite.id)
         mark.destroy
-        redirect_to invite_path(invite)
+        # redirect_to invite_path(invite)
 	end
 
 end
